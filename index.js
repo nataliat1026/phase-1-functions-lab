cfb7d9cfea4c6e95f39932fb5e54b74e324a52f1
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-05 19:06:21 -0400

    commit tree cdd9abe196e6c3f5caa731fcdb7488b0b1060b44

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - hq);
+}
+
+function distanceFromHqInFeet(street) {
+    distanceFromHqInBlocks(street);
+    return (distanceFromHqInBlocks(street) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return ((Math.abs(start - destination)) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    distanceTravelledInFeet(start, destination);
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * 0.02);
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    }
+}
